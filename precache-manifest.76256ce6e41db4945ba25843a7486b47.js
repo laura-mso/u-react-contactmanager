e704936c22332e82f4d6f8f08b06bde2e8d4f1ab
laura-mso <laura.socha@gmx.net>
2019-05-15 14:52:51 +0200

    Updates

---
+++ precache-manifest.76256ce6e41db4945ba25843a7486b47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "89274f3726085725a881",
+    "url": "/u-react-contactmanager/static/css/main.19ae9d77.chunk.css"
+  },
+  {
+    "revision": "89274f3726085725a881",
+    "url": "/u-react-contactmanager/static/js/main.ee6ba66f.chunk.js"
+  },
+  {
+    "revision": "cc515782f8711b2817be",
+    "url": "/u-react-contactmanager/static/js/runtime~main.3ca4daec.js"
+  },
+  {
+    "revision": "f21b56fc39f5d7f4c0c0",
+    "url": "/u-react-contactmanager/static/css/2.2494688b.chunk.css"
+  },
+  {
+    "revision": "f21b56fc39f5d7f4c0c0",
+    "url": "/u-react-contactmanager/static/js/2.a46c82bc.chunk.js"
+  },
+  {
+    "revision": "f1902c2738b2bfae88e2566d2d6d73b5",
+    "url": "/u-react-contactmanager/index.html"
+  }
+];
